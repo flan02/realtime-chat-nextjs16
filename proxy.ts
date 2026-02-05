be1e0599bbdf46729463a56074397e5d7fd97b78
flan02 <chanivetdan@hotmail.com>
2026-02-04 23:58:16 -0300

    libs directory added

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server"
-import { redis } from "./lib/redis"
+import { redis } from "./libs/redis"
 import { nanoid } from "nanoid"
 
 export const proxy = async (req: NextRequest) => {
